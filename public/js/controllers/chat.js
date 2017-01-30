ea05d178aa7cff4aa4ecc1c403410c9876e233d2
Ever Garcia <uriel621@live.com>
2017-01-30 00:39:03 -0800

    connection success

--- public/js/controllers/chat.js
+++ public/js/controllers/chat.js
@@ -1,3 +1,9 @@
 ever.controller("ChatController", ["$scope", "$log", function($scope, $log){
-    console.log("hello There");
-}])
+    $log.info("Your In");
+        var socket;
+        socket = io.connect("http://localhost:3000");
+        console.log(socket);
+        socket.on("MyEvent", function(message){
+            alert(message);
+        });
+}]);
--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 // Include Modules
 var express = require("express");
 var app = express();
-var server = require("http").createServer(app);
+var server = require("http").createServer(app).listen(3000)
 var io = require("socket.io").listen(server);
 
 users = [];
 connections = [];
+
+// server.listen(3000)
 
 // Run The Server
 // server.listen(process.env.PORT || 3000);
@@ -21,11 +23,46 @@ connections = [];
 
 // works css
 
+// app.use(express.static(__dirname + '/public'));
+
+// app.listen(3000, function() {
+//   console.log('Server listening on port 3000');
+// });
+
+// // Open a connection with socket.io
+// io.sockets.on("connection", function(socket){
+//     connections.push(socket);
+//     console.log("Connected: %s sockets connected", connections.length);
+
+//     //Disconnect
+//     socket.on("disconnect", function(data){
+//         connections.splice(connections.indexOf(socket), 1);
+//     console.log("Disconnected: %s sockets connected", connections.length);
+//     }); 
+// });
+
+// ================================================================================
 app.use(express.static(__dirname + '/public'));
 
-app.listen(3000, function() {
-  console.log('Server listening on port 3000');
+app.get("*", function(req, res){
+    res.redirect("public/index.html")
+})
+
+// Open a connection with socket.io
+io.sockets.on("connection", function(socket){
+    // connections.push(socket);
+    console.log("server621");
+    socket.emit("MyEvent", "Connection Success")
+
+    // //Disconnect
+    // socket.on("disconnect", function(data){
+    //     connections.splice(connections.indexOf(socket), 1);
+    // console.log("Disconnected: %s sockets connected", connections.length);
+    // }); 
 });
+
+
+
 
 
 
