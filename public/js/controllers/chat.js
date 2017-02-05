f94ba4ba445f1f9a2a175da551966d3507ea93f0
Ever Garcia <uriel621@live.com>
2017-02-05 02:56:55 -0800

    chat js minor edits

--- public/js/controllers/chat.js
+++ public/js/controllers/chat.js
@@ -1,12 +1,21 @@
 ever.controller("ChatController", ["$scope", function($scope){
   
     var socket = io();
-      $('form').submit(function(){
-        socket.emit('chat message', $('#sending_messages').val());
-        $('#sending_messages').val('');
-        return false;
-      });
       socket.on('chat message', function(msg){
         $('#chat_messages').append($('<li>').text(msg));
       });
+
+      $scope.send_message = function(){
+
+        if(document.getElementById("sending_messages").value === ""){
+          alert("TYPE SOMETHING")
+        }
+        else {
+          socket.emit('chat message', $('#sending_messages').val());
+          $('#sending_messages').val('');
+          return false;
+        }
+
+      }
+      
 }]);
